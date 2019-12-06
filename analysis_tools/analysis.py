040769321609451ee98410a0840bb2f30217a0f0
Mathieu Le Verge--Serandour <mathieu.le-verge-serandour@college-de-france.fr>
2019-12-06 13:56:09 +0100

    Change analysis.py

--- analysis_tools/analysis.py
+++ analysis_tools/analysis.py
@@ -32,26 +32,46 @@ def get_initialconditions(filename, mu=0.6105653703843762, eps = 1e-3) :
     f = open(filename, 'r')
     s = f.readlines()
     f.close()
+    if len(s) == 22 :
+        xis = float(s[5].split(' ')[-1])
+        xiv = float(s[6].split(' ')[-1])
     
-    xis = float(s[5].split(' ')[-1])
-    xiv = float(s[6].split(' ')[-1])
+        L1 = float(s[13].split(' ')[8])
+        N1 = float(s[13].split(' ')[10])
+        L2 = float(s[14].split(' ')[8])
+        N2 = float(s[14].split(' ')[10])
     
-    L1 = float(s[13].split(' ')[8])
-    N1 = float(s[13].split(' ')[10])
-    L2 = float(s[14].split(' ')[8])
-    N2 = float(s[14].split(' ')[10])
+        ell_12 = float(s[18].split(' ')[7])
+        W = int(s[21].split(' ')[-1])
     
-    ell_12 = float(s[18].split(' ')[7])
-    W = int(s[21].split(' ')[-1])
+        L0 = L1+L2+ell_12
+        P1 = L0*eps/L1
+        P2 = L0*eps/L2
     
-    L0 = L1+L2+ell_12
-    P1 = L0*eps/L1
-    P2 = L0*eps/L2
+        C1 = N1 * mu / L1**2
+        C2 = N2 * mu / L2**2
     
-    C1 = N1 * mu / L1**2
-    C2 = N2 * mu / L2**2
+        return xis, xiv, P1, C1, P2, C2, W
+    else : 
+        xis = float(s[5].split(' ')[-1])
+        xiv = float(s[6].split(' ')[-1])
     
-    return xis, xiv, P1, C1, P2, C2, W
+        L1 = float(s[13].split(' ')[8])
+        N1 = float(s[13].split(' ')[10])
+        L2 = float(s[14].split(' ')[8])
+        N2 = float(s[14].split(' ')[10])
+    
+        ell_12 = float(s[18].split(' ')[7])
+        W = 0
+    
+        L0 = L1+L2+ell_12
+        P1 = L0*eps/L1
+        P2 = L0*eps/L2
+    
+        C1 = N1 * mu / L1**2
+        C2 = N2 * mu / L2**2
+    
+        return xis, xiv, P1, C1, P2, C2, W
 
 def get_outputs(filename, list_dir, main_dir = '_data/') :
     L = []
